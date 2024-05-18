1ac186cba043cb96c1e0d320f8ee0cf260dbc6b5
mhjung <mh.jung.dev@gmail.com>
2024-05-18 18:44:47 +0900

    포스트 목록 json 생성 자동화

---
+++ GenerateJsonPostList/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+
+string postsPath = "wwwroot/posts";
+var markdownFiles = Directory.EnumerateFiles(postsPath, "*.md")
+    .Select(Path.GetFileName)
+    .ToList();
+
+var jsonContent = JsonConvert.SerializeObject(markdownFiles, Formatting.Indented);
+File.WriteAllText(Path.Combine(postsPath, "posts.json"), jsonContent);
