e2ed260e9c236f5e116e9d10ff91324596a011d3
mhjung89 <mhjung89@users.noreply.github.com>
2024-05-18 04:40:40 +0000

    deploy: b7915d87ff0dda7aaba093448315af4032153e42

--- Core/Models/Blogs/Post.cs
+++
@@ -1,8 +0,0 @@
-namespace MyBlog.Core.Models.Blogs;
-
-public class Post
-{
-    public string Title { get; set; } = string.Empty;
-    public DateTime Date { get; set; }
-    public string Content { get; set; } = string.Empty;
-}
